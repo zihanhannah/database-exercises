303dedbb5f8032a3c7a3d2ca719eb50ef38f615c
zihan zhao <zihanhannah@gmail.com>
2020-09-04 11:50:43 -0500

    exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(100) NOT NULL,
+name VARCHAR(100) NOT NULL,
+release_date DATE NOT NULL,
+sales FLOAT NOT NULL,
+genre VARCHAR(100) ,
+PRIMARY KEY (id)
+);
+
