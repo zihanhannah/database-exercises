bf4bf0cf9bb76058d9634341c9e1e621360be042
zihan zhao <zihanhannah@gmail.com>
2020-09-23 21:57:26 -0500

    exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,3 +3,17 @@ USE codeup_test_db;
 SELECT name FROM albums
 WHERE artist = 'Pink Floyd';
 
+SELECT release_date FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name , release_date FROM albums
+WHERE release_date  BETWEEN 1990 AND 1999;
+
+SELECT name, sales FROM albums
+WHERE sales < 20;
+
+SELECT name, genre FROM albums
+WHERE genre LIKE '%Rock%' OR genre LIKE '%rock%';
