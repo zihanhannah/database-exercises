f6c20075e79383340f4b07e3d2c3cc6e57a5c08b
zihan zhao <zihanhannah@gmail.com>
2020-09-23 23:52:40 -0500

    exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,6 +1,22 @@
 USE codeup_test_db;
+-- All albums in albums table
+SELECT * FROM albums;
+UPDATE albums SET sales = sales * 10;
+SELECT * FROM albums;
+
 
 SELECT * FROM albums
-UPDATE ablum
+WHERE release_date < 1980;
+UPDATE albums
+SET release_date =1800
+WHERE release_date < 1980;
+SELECT * FROM albums
+WHERE release_date < 1980;
 
 SELECT * FROM albums
+WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson' ;
+SELECT * FROM albums
+WHERE artist = 'Peter Jackson';
